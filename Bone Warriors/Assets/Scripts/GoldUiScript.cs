5cd83e377c3b6b30453d531c6edb68df881d5ab8
tonyCodeMaster <61329413+tonyCodeMaster@users.noreply.github.com>
2020-08-15 13:23:24 +0300

    Gold UI element

---
+++ Bone Warriors/Assets/Scripts/GoldUiScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldUiScript : MonoBehaviour
+{
+    public int playerGold;
+    bool isNotCalled = true;
+    public Text goldTextUi;
+
+    private void Update()
+    {
+        goldTextUi.text = playerGold.ToString() + " Gold";
+
+        if (isNotCalled)
+        {
+            InvokeRepeating("Income", 0f, 2f);
+        }
+
+
+    }
+
+    public void Income()
+    {
+        isNotCalled = false;
+        this.playerGold += 10;
+    }
+}
