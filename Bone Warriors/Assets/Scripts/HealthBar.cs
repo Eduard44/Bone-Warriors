325ad46cc2e7f7c089bced3035d7cb9993bf1b3b
Eduard <bakayaro.ed@gmail.com>
2020-08-16 16:37:42 +0300

    hp bar ui

---
+++ Bone Warriors/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    // the slider is used to adjust the health that we have both min and max
+    public Slider slider;
+
+    //this sets our max health
+    public void SetMaxHealth(int health)
+    {
+        //making the slider have the max health and
+        //of course the min health stays 0 so we don't need a function for it
+        slider.maxValue = health;
+        //this is so we start at max health
+        slider.value = health;
+    }
+    //this function moves our slider which visually moves the values
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
