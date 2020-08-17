ad6f292abe85639856c96bdb7c75b94c06625e04
tonyCodeMaster <61329413+tonyCodeMaster@users.noreply.github.com>
2020-08-17 11:23:37 +0300

    окок

---
+++ Bone Warriors/Assets/Scripts/SummoningGateScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummoningGateScript : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public Transform meleeUnitPrefab;
+
+    public void SummonMeleeUnit()
+    {
+        Instantiate(meleeUnitPrefab, spawnPoint);
+    }
+}
